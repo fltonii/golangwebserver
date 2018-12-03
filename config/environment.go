97edf268cbc7b5f9413b42e9c622efdbb0294705
Joao Tonial <tonialjoao97@gmail.com>
2018-12-03 20:45:18 -0300

    wip

--- config/environment.go
+++ config/environment.go
@@ -22,10 +22,10 @@ func Env() {
 
 // OpenDb open database connection
 func OpenDb() {
-	conf := ("host=" + os.Getenv("HOST") + " port=" + os.Getenv("DBPORT") + " user=" + os.Getenv("USER") + " dbname=" + os.Getenv("DBNAME") + " sslmode=disable password=" + os.Getenv("PASSWORD"))
+	// conf := ("host=" + os.Getenv("HOST") + " port=" + os.Getenv("DBPORT") + " user=" + os.Getenv("USER") + " dbname=" + os.Getenv("DBNAME") + " sslmode=disable password=" + os.Getenv("PASSWORD"))
 	Db, err = gorm.Open(
 		"postgres",
-		conf)
+		"postgres://utelpmgiocqift:131cbb93e197e1d5f673bd703c321dba6b4aa3e95dcf997894b7c400bdadb810@ec2-184-72-239-186.compute-1.amazonaws.com:5432/d2njf86qv85sjm")
 
 	if err != nil {
 		panic("database failed to connect")
