d2b38b668b5d45f09246b50598739718ea03c946
Joao Tonial <tonialjoao97@gmail.com>
2018-12-03 19:32:57 -0300

    adding config file

---
+++ config/environment.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"os"
+
+	"github.com/jinzhu/gorm"
+)
+
+// Db is the global variable that must be used to access the db
+var Db *gorm.DB
+var err error
+
+// Env defines environmental variables
+func Env() {
+	os.Setenv("HOST", "localhost")
+	os.Setenv("DBPORT", "5432")
+	os.Setenv("USER", "tonis")
+	os.Setenv("PASSWORD", "123456")
+	os.Setenv("DBNAME", "golangblog")
+	os.Setenv("PORT", "7070")
+}
+
+// OpenDb open database connection
+func OpenDb() {
+	conf := ("host=" + os.Getenv("HOST") + " port=" + os.Getenv("DBPORT") + " user=" + os.Getenv("USER") + " dbname=" + os.Getenv("DBNAME") + " sslmode=disable password=" + os.Getenv("PASSWORD"))
+	Db, err = gorm.Open(
+		"postgres",
+		conf)
+
+	if err != nil {
+		panic("database failed to connect")
+	}
+}
