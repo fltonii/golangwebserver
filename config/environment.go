dec85726a9318649411632b2fc88e0fa717f5e17
Joao Tonial <tonialjoao97@gmail.com>
2018-12-03 21:12:49 -0300

    fix

--- config/environment.go
+++ config/environment.go
@@ -16,16 +16,16 @@ func Env() {
 	os.Setenv("DBPORT", "5432")
 	os.Setenv("USER", "tonis")
 	os.Setenv("PASSWORD", "123456")
-	os.Setenv("DBNAME", "postgres://utelpmgiocqift:131cbb93e197e1d5f673bd703c321dba6b4aa3e95dcf997894b7c400bdadb810@ec2-184-72-239-186.compute-1.amazonaws.com:5432/d2njf86qv85sjm")
+	os.Setenv("DBNAME", "golangblog")
 	os.Setenv("PORT", "7070")
 }
 
 // OpenDb open database connection
 func OpenDb() {
-	// conf := ("host=" + os.Getenv("HOST") + " port=" + os.Getenv("DBPORT") + " user=" + os.Getenv("USER") + " dbname=" + os.Getenv("DBNAME") + " sslmode=disable password=" + os.Getenv("PASSWORD"))
+	conf := ("host=" + os.Getenv("HOST") + " port=" + os.Getenv("DBPORT") + " user=" + os.Getenv("USER") + " dbname=" + os.Getenv("DBNAME") + " sslmode=disable password=" + os.Getenv("PASSWORD"))
 	Db, err = gorm.Open(
 		"postgres",
-		"postgres://utelpmgiocqift:131cbb93e197e1d5f673bd703c321dba6b4aa3e95dcf997894b7c400bdadb810@ec2-184-72-239-186.compute-1.amazonaws.com:5432/d2njf86qv85sjm")
+		conf)
 
 	if err != nil {
 		panic("database failed to connect")
