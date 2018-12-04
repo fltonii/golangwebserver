fbcd9939e87bd1bf02d400e3731011b278c709e3
Joao Tonial <tonialjoao97@gmail.com>
2018-12-03 21:31:40 -0300

    fix

--- routes/routes.go
+++ routes/routes.go
@@ -9,8 +9,8 @@ import (
 func Routes() (router *mux.Router) {
 	router = mux.NewRouter()
 	router.HandleFunc("/api/posts", controllers.GetPosts).Methods("GET")
-	router.HandleFunc("/api/posts/{id}", controllers.GetPost).Methods("GET")
 	router.HandleFunc("/api/posts", controllers.CreatePost).Methods("POST")
+	router.HandleFunc("/api/posts/{id}", controllers.GetPost).Methods("GET")
 	router.HandleFunc("/api/posts/{id}", controllers.EditPost).Methods("PUT")
 	router.HandleFunc("/api/posts/{id}", controllers.DeletePost).Methods("DELETE")
 	return
